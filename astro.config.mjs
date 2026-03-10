650c69ad516c64eb7fe3c4aa2b4dc2f20cebabf5
Laura Neville <89904035+laura-neville@users.noreply.github.com>
2026-03-10 14:15:47 +0000

    Fix site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: "{https//laura-neville.github.io",
+  site: "{https//laura-neville.github.io",
     plugins: [tailwindcss()],
   },
 });
